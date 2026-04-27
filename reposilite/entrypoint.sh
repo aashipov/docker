0908296e5d1f30f3f72413759be243cd391513fb
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-04-27 19:35:34 +0300

    reposilite fix

--- reposilite/entrypoint.sh
+++ reposilite/entrypoint.sh
@@ -1,12 +1,37 @@
 #!/bin/sh
 
 closure() {
-    local REPOSILITE_WORK_DIR=${REPOSILITE_WORK_DIR:-${HOME}/reposilite/}
-    if [ ! -d ${REPOSILITE_WORK_DIR} ]
+    local CMD_LINE_BUILDER="java"
+    
+    if [ -n "${REPOSILITE_XMX}" ]
     then
+        CMD_LINE_BUILDER="${CMD_LINE_BUILDER} -Xmx${REPOSILITE_XMX}"
+    fi
+
+    CMD_LINE_BUILDER="${CMD_LINE_BUILDER} -jar reposilite*.jar"
+    
+    if [ -n "${REPOSILITE_PORT}" ]
+    then
+        CMD_LINE_BUILDER="${CMD_LINE_BUILDER} --port ${REPOSILITE_PORT}"
+    fi
+
+    if [ -n "${REPOSILITE_WORK_DIR}" ]
+    then
+        CMD_LINE_BUILDER="${CMD_LINE_BUILDER} --working-directory ${REPOSILITE_WORK_DIR}"
         mkdir -p ${REPOSILITE_WORK_DIR}
     fi
-    java -Xmx${REPOSILITE_XMX:-128m} -jar reposilite*.jar --port ${REPOSILITE_PORT:-8088} --working-directory ${REPOSILITE_WORK_DIR} --token ${REPOSILITE_TOKEN:-admin:admin}
+
+    if [ -n "${REPOSILITE_TOKEN}" ]
+    then
+        CMD_LINE_BUILDER="${CMD_LINE_BUILDER} --token ${REPOSILITE_TOKEN}"
+    fi
+
+    if [ -n "${REPOSILITE_DB}" ]
+    then
+        CMD_LINE_BUILDER="${CMD_LINE_BUILDER} --database \"${REPOSILITE_DB}\""
+    fi
+    
+    eval ${CMD_LINE_BUILDER}
 }
 
 closure
---
+++ reposilite/init.sql
@@ -0,0 +1,3 @@
+create role reposilite with login password 'reposilite';
+create database reposilite with owner reposilite template template0 encoding UTF8 LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8';
+--create schema if not exists reposilite authorization reposilite;
